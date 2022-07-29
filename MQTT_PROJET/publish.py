994c6f15696ce37da8eb35cfe18e7d5291f9d1bf
Gaetan-bamongoyo <g.a.bamongoyo@gmail.com>
2022-07-29 12:17:50 +0200

    First commit

---
+++ MQTT_PROJET/publish.py
@@ -0,0 +1,59 @@
+# python 3.6
+
+import ssl
+import time
+from paho.mqtt import client as mqtt_client
+
+
+broker = "broker.emqx.io"
+client_id = "codepo-2022AG"
+topic = "WebToDevice"
+port = 1883
+
+def connect_mqtt():
+    def on_connect(client, userdata, flags, rc):
+        if rc == 0:
+            print("Connected to MQTT Broker!")
+        else:
+            print("Failed to connect, return code %d\n", rc)
+
+    client = mqtt_client.Client(client_id)
+    # client.username_pw_set(username, password)
+    client.on_connect = on_connect
+    client.connect(broker, port)
+    return client
+
+# for i in range(10):
+#     print("debut iteration", i)
+#     print("bonjour")
+#     if i == 2:
+#         break
+#     print("fin iteration", i)
+# print("apres la boucle")
+
+def publish(client):
+    for i in range(1100):
+        msg_count = [{
+            'id':'1',
+            'message':'bienvenu chez toi'
+        }]
+        time.sleep(5)
+        msg = f"messages: {msg_count}"
+        result = client.publish(topic, msg)
+            # result: [0, 1]
+        status = result[0]
+        if status == 0:
+                print(f"Send `{msg}` to topic `{topic}`")
+        else:
+                print(f"Failed to send message to topic {topic}")
+    print("fin boucle")
+
+
+def run():
+    client = connect_mqtt()
+    client.loop_start()
+    publish(client)
+
+
+if __name__ == '__main__':
+    run()
