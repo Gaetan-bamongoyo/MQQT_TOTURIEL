c03e8310f0eaef60cd50c7ae761c7f220db6a23f
Gaetan-bamongoyo <g.a.bamongoyo@gmail.com>
2022-07-30 13:57:29 +0200

    Second commit

--- CELERY_PROJET/celery_project/celeryApp/tasks.py
+++ CELERY_PROJET/celery_project/celeryApp/tasks.py
@@ -2,6 +2,6 @@ from celery import shared_task
 
 @shared_task(bind=True)
 def test_func(self):
-    for i in range(10):
+    for i in range(2):
         print(i)
     return "Done"
--- CELERY_PROJET/celery_project/celeryApp/views.py
+++ CELERY_PROJET/celery_project/celeryApp/views.py
@@ -1,8 +1,15 @@
 from django.shortcuts import render
 from .tasks import test_func
+from django.http import HttpResponse
+from mailApp.tasks import send_mail_func
 
 # Create your views here.
 
 def test(request):
-    test_func.display()
-    return HttpResponse("done")
+    test_func.delay()
+    return HttpResponse("Done")
+
+def send_mail(request):
+    send_mail_func.delay()
+    return HttpResponse("Send")
+    
